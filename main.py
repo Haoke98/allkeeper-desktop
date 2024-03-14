58ac3893c37d023b6cdb530f8e412ac3cc53c494
Sadam·Sadik <1903249375@qq.com>
2024-03-14 13:43:15 +0800

    实现了后台并行webssh服务

--- main.py
+++ main.py
@@ -6,8 +6,26 @@
 @Software: PyCharm
 @disc:
 ======================================="""
+import subprocess
+
 import webview
 
+# 用于存储webssh进程PID的字典
+webssh_pids = {}
+
+
+def start_webssh(session_name):
+    # 启动webssh进程
+    webssh_command = ['wssh', '--port=9080', '--xsrf=False']  # 请根据实际情况替换webssh命令和参数
+    process = subprocess.Popen(webssh_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # 获取并记录PID
+    webssh_pids[session_name] = process.pid
+    # TODO: 写入到文件中进行持久化记录
+    print(f"Started webssh for session '{session_name}'. PID: {process.pid}")
+
+
 if __name__ == '__main__':
+    start_webssh('default')
     webview.create_window('All-Keeper', 'http://127.0.0.1:8001/bupt2018213267@Sdm98/', width=1400, height=1000)
     webview.start()
