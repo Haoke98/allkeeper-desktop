48ce45a11b7c6be4323d8668045482abf161c12d
Sadam·Sadik <1903249375@qq.com>
2024-03-14 10:59:19 +0800

    初步实现了引入pywebview来搭建桌面应用程序架构

---
+++ main.py
@@ -0,0 +1,13 @@
+# _*_ codign:utf8 _*_
+"""====================================
+@Author:Sadam·Sadik
+@Email：1903249375@qq.com
+@Date：2024/3/14
+@Software: PyCharm
+@disc:
+======================================="""
+import webview
+
+if __name__ == '__main__':
+    webview.create_window('All-Keeper', 'http://127.0.0.1:8001/bupt2018213267@Sdm98/', width=1400, height=1000)
+    webview.start()
