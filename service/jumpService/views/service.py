0ea80a7aa9a0a162347b8f672bc33bd9b79a0852
Sadam·Sadik <1903249375@qq.com>
2024-12-07 12:31:49 +0800

    Feat: 实现了服务用户预览页面中的复制用户名, 复制密码,预览密码等功能.
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- service/jumpService/views/service.py
+++ service/jumpService/views/service.py
@@ -8,6 +8,7 @@
 ======================================="""
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.utils.html import escapejs
 
 from ..models import ServiceUser
 
@@ -19,12 +20,66 @@ def get_users(request):
     if serviceId is None:
         return HttpResponse("参数异常/参数缺漏", status=400)
     users = ServiceUser.objects.filter(service_id=serviceId).all()
+    PASSWORD_PLACEHOLDER = '* * * * * * * * * *'
     html_str = '<table border="1">'
-    html_str += "<thead><td>ID</td><td>用户名</td><td>密码</td><td>备注</td></tr></thead>"
+    html_str += "<thead><td>ID</td><td>用户名</td><td>密码</td><td>操作</td><td>备注</td></thead>"
     html_str += "<tbody>"
     for user in users:
-        html_str += "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" % (user.id, user.username,
-                                                                               user.password, user.remark)
-        print(html_str)
-    html_str += "</table>"
+        html_str += '''
+        <tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>
+                <span id='password-display-%s'>%s</span>
+                <span id='password-%s' style='display:none;'>%s</span>
+            </td>
+            <td>
+                <button onclick='copyUsername(\"%s\")'>复制用户名</button>
+                <button onclick='copyPassword(\"%s\")'>复制密码</button>
+                <button onclick='togglePassword(\"%s\")'>预览密码</button>
+            </td>
+            <td>%s</td>
+        </tr>''' % (
+            user.id, escapejs(user.username), user.id, PASSWORD_PLACEHOLDER,user.id, escapejs(user.password), 
+            escapejs(user.username),
+            user.id,
+            user.id,
+            escapejs(user.remark))
+    html_str += "</tbody></table>"
+    html_str += """
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        function copyPassword(userId) {
+            const password = document.getElementById('password-' + userId).innerText;
+            navigator.clipboard.writeText(password).then(() => {
+                alert('密码已复制到剪贴板');
+            }).catch(err => {
+                console.error('复制失败:', err);
+            });
+        }
+
+        function copyUsername(username) {
+            navigator.clipboard.writeText(username).then(() => {
+                alert('用户名已复制到剪贴板');
+            }).catch(err => {
+                console.error('复制失败:', err);
+            });
+        }
+
+        function togglePassword(userId) {
+            const passwordSpan = document.getElementById('password-display-' + userId);
+            const currentDisplay = passwordSpan.innerText;
+            if (currentDisplay === '%s') {
+                passwordSpan.innerText = document.getElementById('password-' + userId).innerText;
+            } else {
+                passwordSpan.innerText = '%s';
+            }
+        }
+
+        window.copyPassword = copyPassword;
+        window.copyUsername = copyUsername;
+        window.togglePassword = togglePassword;
+    });
+    </script>
+    """ % (PASSWORD_PLACEHOLDER, PASSWORD_PLACEHOLDER)
     return HttpResponse(html_str)
