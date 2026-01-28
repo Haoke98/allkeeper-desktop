a6bab3d0f9098191ee22c519588d078c723f847c
Sadam·Sadik <1903249375@qq.com>
2026-01-28 20:25:33 +0800

    Fix: 通过检测命令的方式选择性加载 admin 部分，解决了 migrate 等数据库相关命令及其他命令的正常执行受到干扰

--- service/jumpService/admin/__init__.py
+++ service/jumpService/admin/__init__.py
@@ -6,17 +6,21 @@
 @Software: PyCharm
 @disc:
 ======================================="""
-from .bt import BtAdmin
-from .dbService import DbServiceAdmin
-from .dbServiceUser import DbServiceUserAdmin
-from .device import DeviceAdmin
-from .es import ElasticSearchAdmin
-from .minio import MinIOAdmin
-from .net import NetWorkAdmin, NetDeviceAdmin, IPAddressAdmin
-from .router import RouterAdmin
-from .server import ServerAdmin
-from .service import ServiceAdmin, ServiceUserAdmin, ServiceTypeAdmin
-from .ssh import SystemUserAdmin
-from .operation_system import OperationSystemAdmin
-from .channel import ChannelAdmin
-from .port import PortMapAdmin, PortAdmin
+import sys
+
+# 永久解决：如果是迁移相关命令，直接跳过 Admin 加载，避免触发数据库查询
+if not any(arg in sys.argv for arg in ['makemigrations', 'migrate', 'inspectdb']):
+    from .bt import BtAdmin
+    from .dbService import DbServiceAdmin
+    from .dbServiceUser import DbServiceUserAdmin
+    from .device import DeviceAdmin
+    from .es import ElasticSearchAdmin
+    from .minio import MinIOAdmin
+    from .net import NetWorkAdmin, NetDeviceAdmin, IPAddressAdmin
+    from .router import RouterAdmin
+    from .server import ServerAdmin
+    from .service import ServiceAdmin, ServiceUserAdmin, ServiceTypeAdmin
+    from .ssh import SystemUserAdmin
+    from .operation_system import OperationSystemAdmin
+    from .channel import ChannelAdmin
+    from .port import PortMapAdmin, PortAdmin
